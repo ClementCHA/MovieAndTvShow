74fb6555b5c5fe8da3216b8dea20112cf520a2fe
Clement CHARLES <clement.charles@origins-digital.com>
2022-04-25 22:58:42 +0200

    Simple search engine for series and movie with IMDB Api

--- src/MainPage/container.jsx
+++ src/MainPage/container.jsx
@@ -7,22 +7,46 @@ const MainPageContainer = () => {
   const [choice, setChoice] = useState("");
   const [movies, setMovies] = useState([]);
   const [tvShows, setTvShows] = useState([]);
+  const [activeSearch, setActiveSearch] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   const makeYourChoice = async (choice) => {
     setChoice(choice);
 
-    if (choice === "moovie") {
+    if (choice === "Movies") {
       const fetchedMoovie = await axios(
         `https://imdb-api.com/en/API/Top250Movies/${REACT_APP_IMDB_API_KEY}`
       );
       setMovies(fetchedMoovie.data.items);
     }
-    if (choice === "tvshow") {
+    if (choice === "Tv-Shows") {
       const fecthedSeries = await axios(
         `https://imdb-api.com/en/API/Top250TVs/${REACT_APP_IMDB_API_KEY}`
       );
+
       setTvShows(fecthedSeries.data.items);
     }
+  };
+
+  const resetChoice = () => {
+    setChoice("");
+    setMovies([]);
+    setTvShows([]);
+    setActiveSearch([]);
+  };
+
+  const createPersonnalChoice = async (search) => {
+    const findPersonnalChoice = await axios(
+      `https://imdb-api.com/en/API/Search/k_h8n0x2p8/${search}`
+    );
+
+    setActiveSearch(findPersonnalChoice);
+
+    /* console.log(findPersonnalChoice); */
+  };
+
+  const onChangeInputValue = (e) => {
+    setInputValue(e.target.value);
   };
 
   return (
@@ -31,6 +55,11 @@ const MainPageContainer = () => {
       makeYourChoice={makeYourChoice}
       movies={movies}
       tvShows={tvShows}
+      resetChoice={resetChoice}
+      createPersonnalChoice={createPersonnalChoice}
+      activeSearch={activeSearch}
+      inputValue={inputValue}
+      onChangeInputValue={onChangeInputValue}
     />
   );
 };
--- src/MainPage/view.jsx
+++ src/MainPage/view.jsx
@@ -1,38 +1,115 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Loupe from "./306102.svg";
 import "./style.scss";
 
-const MainPageView = ({ choice, makeYourChoice, movies, tvShows }) => {
+const MainPageView = ({
+  choice,
+  makeYourChoice,
+  movies,
+  tvShows,
+  resetChoice,
+  createPersonnalChoice,
+  activeSearch,
+  inputValue,
+  onChangeInputValue,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createPersonnalChoice(inputValue);
+  };
+
   return (
     <>
       {!choice && (
         <div className="choosePgrm">
           <p className="title">What are you looking for ?</p>
           <div className="choices">
-            <p onClick={() => makeYourChoice("moovie")} className="choice">
+            <p onClick={() => makeYourChoice("Movies")} className="choice">
               Moovie
             </p>{" "}
-            <p onClick={() => makeYourChoice("tvshow")} className="choice">
+            <p onClick={() => makeYourChoice("Tv-Shows")} className="choice">
               Tv Show
             </p>
           </div>
         </div>
       )}
+      {choice && (
+        <>
+          <div className="searchPage">
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <input
+                className="searchBar"
+                type="text"
+                name="name"
+                value={inputValue}
+                placeholder={`${choice}`}
+                onChange={onChangeInputValue}
+              />
+              <button
+                className="submitButton"
+                type="submit"
+                disabled={inputValue.length > 0 ? false : true}
+              >
+                <img
+                  style={{
+                    margin: "-0.5em",
+                    width: "2em",
+                    height: "auto",
+                    filter: "invert(1)",
+                  }}
+                  src={Loupe}
+                  alt="loupe"
+                />
+              </button>
+            </form>
+          </div>
+          <button onClick={resetChoice} className="return">
+            Return To Main Page
+          </button>
+          {activeSearch.length === 0 && (
+            <div className="cards">
+              {(movies.length > 0 ? movies : tvShows).map((pgrm) => {
+                return (
+                  <div
+                    key={pgrm.rank}
+                    className="card"
+                    onClick={() =>
+                      window.open(
+                        `https://www.imdb.com/find?q=${pgrm.title}`,
+                        "_blank"
+                      )
+                    }
+                  >
+                    <h1 className="cardTitle">
+                      {pgrm.title.toUpperCase()} -{" "}
+                      <span className="imdNote">{pgrm.imDbRating}/10</span>
+                    </h1>
+                    <img
+                      className="cardImage"
+                      src={pgrm.image}
+                      alt="filmImage"
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </>
+      )}
     </>
-    /*    <input
-        className="searchBar"
-        type="text"
-        name="name"
-        placeholder=" moovie or TV Show "
-        onChange={() => console.log("test")}
-      /> */
   );
 };
 
 MainPageView.prototypes = {
   choice: PropTypes.string.isRequired,
   makeYourChoice: PropTypes.func.isRequired,
+  resetChoice: PropTypes.func.isRequired,
+  createPersonnalChoice: PropTypes.func.isRequired,
   movies: PropTypes.array,
   tvShows: PropTypes.array,
+  activeSearch: PropTypes.array,
+  inputValue: PropTypes.string.isRequired,
+  onChangeInputValue: PropTypes.func.isRequired,
 };
 export default MainPageView;
